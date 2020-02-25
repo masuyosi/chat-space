3a72fe44cfcee2a36960ab2715973f6e851a048b
masuyosi <weiss.der.fulss@gmail.com>
2020-02-25 16:47:33 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,15 @@
 class MessagesController < ApplicationController
+
   def index
+  end
+
+  def create
+    message = Messages.create(message_params)
+    redirect_to group_messages_path
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body).merge(user_id: current_user.id, message_id: params[:message_id])
   end
 end
